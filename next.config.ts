0cea94524a33c4bb00d5728c656e85b76e8f6ed5
Absar Ahmad Rao <ahmadraoabsar@gmail.com>
2025-07-28 15:03:48 +0000

    ./src/app/(app)/layout.tsx
    ./src/components/notification-handler.tsx
    At

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   /* config options here */
-  experimental: {
-    // This is required to suppress the warning about winston-transport not being found.
-    // It is a dependency of a dependency and not used in the app.
-    serverComponentsExternalPackages: ['@opentelemetry/winston-transport', 'handlebars'],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
