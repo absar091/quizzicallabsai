c5f489e0dbac44f2c8c0ed816f46f208561e2aac
Absar Ahmad Rao <ahmadraoabsar@gmail.com>
2025-07-28 12:23:10 +0000

    Error: Failed to load chunk /_next/static/chunks/node_modules_b152cf6a._

--- src/next.config.ts
+++ src/next.config.ts
@@ -4,9 +4,11 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'standalone',
   /* config options here */
-  // This is required to suppress warnings about some packages not being found.
-  // They are dependencies of dependencies and not used directly in the app.
-  serverExternalPackages: ['@opentelemetry/winston-transport', 'handlebars'],
+  experimental: {
+    // This is required to suppress the warning about winston-transport not being found.
+    // It is a dependency of a dependency and not used in the app.
+    serverComponentsExternalPackages: ['@opentelemetry/winston-transport', 'handlebars'],
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
