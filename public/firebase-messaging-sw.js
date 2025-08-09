d261b72384074da19f47b65f3cff3eef02460151
Absar Ahmad Rao <ahmadraoabsar@gmail.com>
2025-08-09 15:56:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er modified:   public/firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,31 +1,33 @@
 
-// Import the Firebase app and messaging packages
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+// This file needs to be in the public directory
+
+// Scripts for Firebase products
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCzRpRNFBAodjKhmmJAMvaBiDNH9-vK1Yg",
-  authDomain: "quizzical-ai.firebaseapp.com",
-  databaseURL: "https://quizzical-ai-default-rtdb.firebaseio.com",
-  projectId: "quizzical-ai",
-  storageBucket: "quizzical-ai.appspot.com",
+  authDomain: "quizzicallab-ai.firebaseapp.com",
+  databaseURL: "https://quizzicallab-ai-default-rtdb.firebaseio.com",
+  projectId: "quizzicallab-ai",
+  storageBucket: "quizzicallab-ai.appspot.com",
   messagingSenderId: "208281807503",
-  appId: "1:208281807503:web:8130ddca90c6068a7f1efa"
+  appId: "1:208281807503:web:6185af3a3b1c80ee7f1efa"
 };
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Optional: Set up a background message handler
+// Optional: To handle messages when your app is in the background or closed
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon || '/icon.svg'
+    icon: '/icon.svg'
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
