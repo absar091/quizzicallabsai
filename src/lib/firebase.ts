4f7e31552b360b39068ec2292da9fad0ebf30b90
Absar Ahmad Rao <ahmadraoabsar@gmail.com>
2025-08-31 08:09:55 +0000

    update all varibales iwth it // Import the functions you need from the S

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { 
-    getAuth, 
-    initializeAuth, 
-    indexedDBLocalPersistence, 
-    browserLocalPersistence, 
-    browserSessionPersistence,
-    GoogleAuthProvider,
-    connectAuthEmulator
+    getAuth
 } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
